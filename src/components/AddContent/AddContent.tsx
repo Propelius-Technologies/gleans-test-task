531baac070da2cddd97b908237663fdf08b27dc3
Nishit Patel <nishit.patel@propelius.tech>
2023-08-09 11:00:15 +0530

    adds tags & collections

--- src/components/AddContent/AddContent.tsx
+++ src/components/AddContent/AddContent.tsx
@@ -13,7 +13,7 @@ const AddContent = () => {
 
   return (
     <div className='h-full w-full'>
-      <div className='fixed top-0 left-0 right-0 bottom-0 animate-blur' />
+      <div className='fixed top-0 left-0 right-0 bottom-0 backdrop-blur-3xl' />
       <AnimatePresence mode='wait'>
         {activeStep === 1 && (
           <motion.div
--- src/services/content/content.services.ts
+++ src/services/content/content.services.ts
@@ -6,16 +6,37 @@ import { Content } from 'src/types/model';
 export const addContent = async (content: Content) => {
   const meta = getLinkMeta();
   try {
-    await supabase
+    const res = await supabase
       .from('gleans')
-      .insert([{
-        title: content.title,
-        description: content.description,
-        link: meta.url || '',
-        image: content.image || '',
-      }])
+      .insert([
+        {
+          title: content.title,
+          description: content.description,
+          link: meta.url || '',
+          image: content.image || '',
+        },
+      ])
       .select()
       .throwOnError();
+    if (res.data) {
+      await supabase
+        .from('tag_gleans')
+        .insert(
+          content.tags.map((tag) => ({
+            tag_id: tag.id,
+            glean_id: res.data[0].id,
+          }))
+        );
+
+      await supabase
+        .from('collection_glean')
+        .insert(
+          content.collections.map((collection) => ({
+            collection_id: collection.id,
+            glean_id: res.data[0].id,
+          }))
+        );
+    }
     return { success: true };
   } catch (error) {
     return { success: false };
